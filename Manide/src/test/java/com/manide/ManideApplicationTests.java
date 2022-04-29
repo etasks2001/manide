40089d74dc45ebc89e874943d77af975b7489e03
satells <msergiost@hotmail.com>
2022-04-29 10:39:41 -0300

    Start.

---
+++ Manide/src/test/java/com/manide/ManideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
