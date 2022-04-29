cf4c4d6e2c9d1e09565057a5209363a637f18278
satells <msergiost@hotmail.com>
2022-04-29 10:44:23 -0300

    Start

---
+++ manide/src/main/java/com/manide/manide/ManideApplication.java
@@ -0,0 +1,13 @@
+package com.manide.manide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManideApplication.class, args);
+	}
+
+}
