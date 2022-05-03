3d64036985a954b5f85b0b5d61db6c0238b1a4ad
satells <msergiost@hotmail.com>
2022-05-03 16:56:19 -0300

    .

--- manide/src/main/java/com/manide/util/Config.java
+++ manide/src/main/java/com/manide/util/Config.java
@@ -6,6 +6,7 @@ import static javax.xml.bind.Marshaller.JAXB_FRAGMENT;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -30,6 +31,15 @@ public class Config {
 	marshaller.setProperty(JAXB_FRAGMENT, TRUE);
 	marshaller.setProperty(XML_HEADERS, XML_VERSION);
 	return marshaller;
+    }
+
+    @Bean
+    Unmarshaller createUnmarshaller() throws JAXBException {
+	Unmarshaller unmarshaller = jaxbContext().createUnmarshaller();
+
+//	unmarshaller.setProperty(JAXB_FRAGMENT, TRUE);
+//	unmarshaller.setProperty(XML_HEADERS, XML_VERSION);
+	return unmarshaller;
     }
 
 }
--- manide/src/test/java/com/manide/xml/evento/entrada/TEnvEventoTest.java
+++ manide/src/test/java/com/manide/xml/evento/entrada/TEnvEventoTest.java
@@ -4,12 +4,18 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 import javax.xml.bind.JAXB;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
@@ -45,64 +51,78 @@ class TEnvEventoTest {
 
     @Autowired
     Marshaller marshaller;
+    @Autowired
+    Unmarshaller unmarshaller;
 
     @Autowired
     private FactoryXmlTest factoryXml;
 
     @Test
     @DisplayName("Teste XML gravado")
-    void test() throws Exception {
+    void test() {
 
-	TEvento createEvento2 = factoryXml.createEvento2();
-	TEvento createEvento1 = factoryXml.createEvento1();
+	try {
+	    TEvento createEvento2 = factoryXml.createEvento2();
+	    TEvento createEvento1 = factoryXml.createEvento1();
 
-	OutputStream osEvent1 = new FileOutputStream(FILE_NAME_EVENTO_1);
-	marshaller.marshal(objectFactory.createEvento(createEvento1), osEvent1);
-	osEvent1.close();
+	    OutputStream osEvent1 = new FileOutputStream(FILE_NAME_EVENTO_1);
+	    marshaller.marshal(objectFactory.createEvento(createEvento1), osEvent1);
+	    osEvent1.close();
 
-	FileOutputStream osEvent2 = new FileOutputStream(FILE_NAME_EVENTO_2);
-	marshaller.marshal(objectFactory.createEvento(createEvento2), osEvent2);
-	osEvent2.close();
+	    FileOutputStream osEvent2 = new FileOutputStream(FILE_NAME_EVENTO_2);
+	    marshaller.marshal(objectFactory.createEvento(createEvento2), osEvent2);
+	    osEvent2.close();
 
-	/* ====================================================== */
-	NFeXmlSignature certClient = new NFeXmlSignature("C:\\MDe\\fs.jks", "448006".toCharArray(), "infEvento");
+	    /* ====================================================== */
+	    NFeXmlSignature certClient = new NFeXmlSignature("C:\\MDe\\fs.jks", "448006".toCharArray(), "infEvento");
 
-	certClient.signXML(FILE_NAME_EVENTO_1, FILE_NAME_EVENTO_1_SIGNED);
-	certClient.signXML(FILE_NAME_EVENTO_2, FILE_NAME_EVENTO_2_SIGNED);
+	    certClient.signXML(FILE_NAME_EVENTO_1, FILE_NAME_EVENTO_1_SIGNED);
+	    certClient.signXML(FILE_NAME_EVENTO_2, FILE_NAME_EVENTO_2_SIGNED);
 
-	/* ====================================================== */
+	    /* ====================================================== */
 
-	InputStream isSigned1 = new FileInputStream(FILE_NAME_EVENTO_1_SIGNED);
-	InputStream isSigned2 = new FileInputStream(FILE_NAME_EVENTO_2_SIGNED);
+	    InputStream isSigned1 = new FileInputStream(FILE_NAME_EVENTO_1_SIGNED);
+	    InputStream isSigned2 = new FileInputStream(FILE_NAME_EVENTO_2_SIGNED);
 
 //	TEvento evento1 = JAXB.unmarshal(FILE_NAME_EVENTO_1_SIGNED, TEvento.class);
 //	TEvento evento2 = JAXB.unmarshal(FILE_NAME_EVENTO_2_SIGNED, TEvento.class);
 
-	TEvento evento1 = JAXB.unmarshal(isSigned1, TEvento.class);
-	TEvento evento2 = JAXB.unmarshal(isSigned2, TEvento.class);
+	    JAXBElement<TEvento> evento1 = (JAXBElement<TEvento>) unmarshaller.unmarshal(new StreamSource(isSigned1), TEvento.class);
+	    JAXBElement<TEvento> evento2 = (JAXBElement<TEvento>) unmarshaller.unmarshal(new StreamSource(isSigned2), TEvento.class);
 
-	System.out.println(evento1.getSignature());
-	System.out.println(evento2.getSignature());
+	    System.out.println(evento1.getValue().getSignature());
+	    System.out.println(evento2.getValue().getSignature());
 
-	TEnvEvento envEvento = factoryXml.createEvent();
-	envEvento.addEvento(evento1);
-	envEvento.addEvento(evento2);
+	    TEnvEvento envEvento = factoryXml.createEvent();
 
-	OutputStream os = new FileOutputStream(FILE_NAME_ENVIO_EVENTO);
+	    envEvento.addEvento(evento1.getValue());
+	    envEvento.addEvento(evento2.getValue());
 
-	marshaller.marshal(objectFactory.createEnvEvento(envEvento), os);
-	os.close();
+	    OutputStream os = new FileOutputStream(FILE_NAME_ENVIO_EVENTO);
 
-	/* ====================================================== */
+	    marshaller.marshal(objectFactory.createEnvEvento(envEvento), os);
+	    os.close();
 
-	InputStream is = new FileInputStream(FILE_NAME_ENVIO_EVENTO);
-	TEnvEvento envEvento2 = JAXB.unmarshal(is, TEnvEvento.class);
-	is.close();
+	    /* ====================================================== */
 
-	assertThat(envEvento.getIdLote(), is(envEvento2.getIdLote()));
-	assertThat(envEvento.getVersao(), is(envEvento2.getVersao()));
+	    InputStream is = new FileInputStream(FILE_NAME_ENVIO_EVENTO);
+	    TEnvEvento envEvento2 = JAXB.unmarshal(is, TEnvEvento.class);
+	    is.close();
 
-	assertThat(envEvento.getEvento().size(), is(2));
+	    assertThat(envEvento.getIdLote(), is(envEvento2.getIdLote()));
+	    assertThat(envEvento.getVersao(), is(envEvento2.getVersao()));
+
+	    assertThat(envEvento.getEvento().size(), is(2));
+	} catch (FileNotFoundException e) {
+	    e.printStackTrace();
+	} catch (JAXBException e) {
+	    e.printStackTrace();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	} catch (Exception e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
 
     }
 }
