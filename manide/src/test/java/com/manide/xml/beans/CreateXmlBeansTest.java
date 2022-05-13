bec3499bbad68298c023f36304b87dcbc9a92290
satells <msergiost@hotmail.com>
2022-05-13 13:03:28 -0300

    .

--- manide/src/test/java/com/manide/xml/beans/CreateXmlBeansTest.java
+++ manide/src/test/java/com/manide/xml/beans/CreateXmlBeansTest.java
@@ -2,48 +2,77 @@ package com.manide.xml.beans;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+
+import javax.xml.namespace.QName;
 
 import org.apache.xmlbeans.XmlCursor;
+import org.apache.xmlbeans.XmlOptions;
 import org.junit.jupiter.api.Test;
 
 import br.inf.portalfiscal.nfe.EnvEventoDocument;
+import br.inf.portalfiscal.nfe.TAmb;
+import br.inf.portalfiscal.nfe.TCOrgaoIBGE;
 import br.inf.portalfiscal.nfe.TEnvEvento;
+import br.inf.portalfiscal.nfe.TEvento;
+import br.inf.portalfiscal.nfe.TEvento.InfEvento;
+import br.inf.portalfiscal.nfe.TEvento.InfEvento.TpEvento;
 
 class CreateXmlBeansTest {
+    private static final QName[] PROPERTY_QNAME = { new QName("http://www.portalfiscal.inf.br/nfe", "evento"), };
 
     @Test
     void test() {
 
 	try {
 
-	    File file = new File("C:\\MDe\\envioXMLBeans.xml");
-	    EnvEventoDocument envEventoDocument = EnvEventoDocument.Factory.newInstance();
+	    File fileEnvEvento = new File("C:\\MDe\\envioXMLBeans.xml");
+	    File fileEvento = new File("C:\\MDe\\eventoXMLBeans.xml");
+	    EnvEventoDocument document = EnvEventoDocument.Factory.newInstance();
+
 	    TEnvEvento envEvento = TEnvEvento.Factory.newInstance();
 
+	    TEvento evento = TEvento.Factory.newInstance();
+
+	    evento.setVersao("1.00");
+
+	    InfEvento infEvento = InfEvento.Factory.newInstance();
+	    infEvento.setId("123123123");
+	    infEvento.setCOrgao(TCOrgaoIBGE.X_35);
+	    infEvento.setTpAmb(TAmb.X_2);
+	    infEvento.setCNPJ("11222333000199");
+	    infEvento.setChNFe("01234567890123456789012345678901234567891234");
+	    infEvento.setDhEvento("AAAA-MM-DDThh:mm:ssTZD-03:00");
+	    infEvento.setTpEvento(TpEvento.X_210200);
+	    infEvento.setNSeqEvento("1");
+
+	    evento.setInfEvento(infEvento);
+
+	    envEvento.setEventoArray(new TEvento[] { evento });
 	    envEvento.setVersao("1.00");
 	    envEvento.setIdLote("1");
 
 	    XmlCursor cursor = envEvento.newCursor();
 
-	    Map<String, String> addToThis = new HashMap<String, String>();
-
-	    cursor.getAllNamespaces(addToThis);
-	    cursor.prefixForNamespace("http://www.portalfiscal.inf.br/nfe");
 	    cursor.toNextToken();
 	    cursor.insertNamespace("", "http://www.portalfiscal.inf.br/nfe");
 	    cursor.dispose();
 
-	    envEventoDocument.setEnvEvento(envEvento);
+	    document.setEnvEvento(envEvento);
 
-	    envEventoDocument.save(file);
+	    document.save(fileEnvEvento);
+
+	    XmlOptions options = new XmlOptions();
+	    options.setSaveSyntheticDocumentElement(new QName("http://www.portalfiscal.inf.br/nfe", "evento"));
+
+	    XmlCursor cursor2 = evento.newCursor();
+
+	    cursor2.toNextToken();
+	    cursor2.insertNamespace("", "http://www.portalfiscal.inf.br/nfe");
+	    cursor2.dispose();
+	    evento.save(fileEvento, options);
 
 	} catch (IOException e) {
-	    // TODO Auto-generated catch block
 	    e.printStackTrace();
 	}
-
     }
-
 }
