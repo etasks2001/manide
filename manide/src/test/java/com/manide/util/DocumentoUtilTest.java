16f2fcc1b8c2da6ed2ecc66338fad35646d8dd86
satells <msergiost@hotmail.com>
2022-05-23 16:51:44 -0300

    .

--- manide/src/test/java/com/manide/util/DocumentoUtilTest.java
+++ manide/src/test/java/com/manide/util/DocumentoUtilTest.java
@@ -1,15 +1,24 @@
 package com.manide.util;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
 
 import org.junit.jupiter.api.Test;
 import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 
 class DocumentoUtilTest {
 
@@ -20,12 +29,20 @@ class DocumentoUtilTest {
 	    String xml = util.xmlAssinado();
 	    System.out.println(xml);
 
-	    OutputStream os = new FileOutputStream(new File("c:/mde/teste-assinado.xml"));
-	    OutputStreamWriter outputStreamWriter = new OutputStreamWriter(os, "UTF-8");
-	    outputStreamWriter.write(xml);
+	    Transformer transformer = getTransformer();
+	    OutputStream os = new FileOutputStream("c:/mde/teste-assinado.xml");
+	    DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+	    String constant = "http://www.w3.org/2001/XMLSchema";
+	    SchemaFactory xsdFactory = SchemaFactory.newInstance(constant);
+	    Schema schema = xsdFactory.newSchema(getClass().getResource("/Evento_ManifestaDest_PL_v1.01/envConfRecebto_v1.00.xsd"));
+	    documentBuilderFactory.setSchema(schema);
+	    documentBuilderFactory.setNamespaceAware(true);
+	    documentBuilderFactory.setValidating(false);
 
-	    outputStreamWriter.flush();
-	    outputStreamWriter.close();
+	    Document documentAssinado = documentBuilderFactory.newDocumentBuilder().parse(new InputSource(new InputStreamReader(new ByteArrayInputStream(xml.getBytes()))));
+
+	    transformer.transform(new DOMSource(documentAssinado), new StreamResult(os));
+	    os.close();
 
 	} catch (Exception e) {
 	    e.printStackTrace();
@@ -51,4 +68,11 @@ class DocumentoUtilTest {
 	}
 
     }
+
+    private Transformer getTransformer() throws TransformerConfigurationException {
+	TransformerFactory tf = TransformerFactory.newInstance();
+	Transformer transformer = tf.newTransformer();
+	return transformer;
+    }
+
 }
